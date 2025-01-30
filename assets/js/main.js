e14d090597631ab23f82b28b7b0c1a31ba0067bc
al-jabir <aljabir098@gmail.com>
2025-01-30 21:25:37 +0600

    feat: refactor WOW animation initialization into a dedicated function

--- assets/js/main.js
+++ assets/js/main.js
@@ -236,7 +236,16 @@
     },
   });
 
-  // animation
+  // wow animation
 
-  new WOW().init();
+  function wowAnimation() {
+    var wow = new WOW({
+      boxClass: 'wow',
+      animateClass: 'animated',
+      offset: 0,
+      mobile: false,
+      live: true,
+    });
+    wow.init();
+  }
 })(jQuery);
